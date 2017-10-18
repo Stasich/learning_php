afa60f42894411fbc7bf33450bc63bac5dffddf4
Stas <sromanenko91@gmail.com>
2017-10-18 20:38:10 +0300

    add insertComment(), getComment()

---
+++ commenting_system/model.php
@@ -0,0 +1,19 @@
+<?php
+function getComments( $pdo )
+{
+    $query = "SELECT name, text, time FROM comments ORDER BY id ASC"; //запрос комментариев из базы
+    $stmt = $pdo->query($query);
+    return $stmt;
+}
+
+function insertComment( $name, $comment, $pdo )
+{
+    $time = time(); //получаю текущее время
+    $query = "INSERT INTO comments (name, text, time) VALUES ( :name, :comment, :time )"; //строка для запроса
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([
+        'name' => $name,
+        'comment' => $comment,
+        'time' => $time
+    ]);
+}
