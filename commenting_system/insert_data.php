da9545aee85525ff557987cf6bdbb4c4fcf2ce16
Stas <sromanenko91@gmail.com>
2017-10-18 18:18:47 +0300

    Добавил обработку данных из формы(htmlentities)

--- commenting_system/insert_data.php
+++ commenting_system/insert_data.php
@@ -1,7 +1,11 @@
 <?php
-$time = time();         //получаю текущее время
+$time = time();                 //получаю текущее время
 $name = $_POST['first_name'];
 $text = $_POST['comment'];
+
+$name = htmlentities($name);   //
+$text = htmlentities($text);
+
 $query = "INSERT INTO comments (name, text, time) VALUES ( :name, :text, :time )"; //строка для запроса
 $stmt = $pdo->prepare($query);
 $stmt->execute([
