3a5d07bd50e4324930cc1b7c402f796b08cbb9ed
Stas <sromanenko91@gmail.com>
2017-10-18 20:37:26 +0300

    подключил файл 'model.php' с функциями

--- commenting_system/index.php
+++ commenting_system/index.php
@@ -1,8 +1,13 @@
 <?php
-require("connect_to_mysql.php");  //подключение к базе
+require_once('connect_to_mysql.php');  //подключение к базе
+require_once ('model.php');
 
-if ($_POST) {                     //если есть данные в POST запросе до добаить их в базу
-    require("insert_data.php");
+if ($_POST) {                           //если есть данные в POST запросе до добаить их в базу
+    $name = htmlentities( $_POST['first_name'] );
+    $comment = htmlentities( $_POST['comment'] );
+    insertComment( $name, $comment, $pdo);
 }
+
+$stmt = getComments( $pdo );
 
 require_once ("html.php");        // html страничка
